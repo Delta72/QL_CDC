0123d264f19984f85602259323a856cfc6c3e80e
Phong Bao Tran <76481497+Delta72@users.noreply.github.com>
2022-06-08 13:40:05 +0700

    Them ctrl GioHang

---
+++ QL_CDC/Controllers/GioHangController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QL_CDC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace QL_CDC.Controllers
+{
+    public class GioHangController : Controller
+    {
+        QL_CDCContext db = new QL_CDCContext();
+        public IActionResult HienSanPhamTrongGio()
+        {
+            var sp = 0;
+            if (User.Identity.IsAuthenticated)
+            {
+                string tk = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                sp = db.GIOHANG.Count(a => a.SV_MSSV == tk);
+            }
+            return Json(sp);
+        }
+
+        public IActionResult GioHang()
+        {
+            return View();
+        }
+    }
+}
